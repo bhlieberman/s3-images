439cd589859ed8583a311e3d3a0ff79e89fc6687
blieberman@healthcareanalytics.expert <blieberman@healthcareanalytics.expert>
2022-11-22 11:34:42 -0700

    adding jar

--- build.clj
+++ build.clj
@@ -2,10 +2,10 @@
   (:require [clojure.tools.build.api :as b]))
 
 (def lib 'com.bhlieberman/s3.client)
-#_(def version (format "1.2.%s" (b/git-count-revs nil)))
+(def version (format "0.0.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
-(def jar-file (format "target/%s-%s.jar" (name lib) "0.0.1"))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
 
 (defn clean [_]
   (b/delete {:path "target"}))
@@ -13,7 +13,7 @@
 (defn jar [_]
   (b/write-pom {:class-dir class-dir
                 :lib lib
-                :version "0.0.1"
+                :version version
                 :basis basis
                 :src-dirs ["src"]})
   (b/copy-dir {:src-dirs ["src" "resources"]
